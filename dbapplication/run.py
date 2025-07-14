4a422e4d9f34db2838f2c7f1d88d5526d17d2930
Ronda <johnronda0527@gmail.com>
2025-07-14 21:40:13 +0800

    created different files

---
+++ dbapplication/run.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+
+def create_app():
+        app = Flask(__name__, template_folder='templates')
+        app.config['SQLACADEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
+
+        db.init_app(app)
