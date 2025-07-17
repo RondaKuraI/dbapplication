a905238fe456350f00f2712b18932680617ae042
Ronda <johnronda0527@gmail.com>
2025-07-18 00:21:51 +0800

    installed sqlalchemy and migrate

--- dbapplication/app.py
+++ dbapplication/app.py
@@ -6,11 +6,12 @@ db = SQLAlchemy()
 
 def create_app():
         app = Flask(__name__, template_folder='templates')
-        app.config['SQLACADEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
 
         db.init_app(app)
 
-        #imports later on
+        from routes import register_routes
+        register_routes(app, db)
 
         migrate = Migrate(app, db)
 
--- dbapplication/models.py
+++ dbapplication/models.py
@@ -1,6 +1,6 @@
 from app import db
 
-class Person():
+class Person(db.Model):
         __tablename__ = 'people'
 
         pid = db.Column(db.Integer, primary_key=True)
@@ -9,4 +9,4 @@ class Person():
         job = db.Column(db.Text)
 
         def __repr__(self):
-                return f'Person with name {self.name} and age {self.age}'
+                return f'Person with name {self.name} and age {self.age }'
